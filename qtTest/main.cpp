d357f9af0ef749d50bd340254367d09ba2b90b80
René Bohne <rene.bohne@gmail.com>
2013-10-31 15:54:20 +0100

    Update main.cpp
    People are having problems with the default stack size...
    maybe this helps?? (UNTESTED)
    see here: http://stackoverflow.com/questions/2279052/increase-stack-size-in-linux-with-setrlimit

--- qtTest/main.cpp
+++ qtTest/main.cpp
@@ -1,8 +1,27 @@
 #include <QtWidgets>
 #include "mainwindow.h"
+#include <sys/resource.h>
 
 int main(int argc, char *argv[])
 {
+    const rlim_t kStackSize = 64L * 1024L * 1024L;   // min stack size = 64 Mb
+    struct rlimit rl;
+    int result;
+
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result == 0)
+    {
+        if (rl.rlim_cur < kStackSize)
+        {
+            rl.rlim_cur = kStackSize;
+            result = setrlimit(RLIMIT_STACK, &rl);
+            if (result != 0)
+            {
+                fprintf(stderr, "setrlimit returned result = %d\n", result);
+            }
+        }
+    }
+    
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
